ba7355a1152efead5a15cab399b06bacf1422311
27X19 <taanas96@gmail.com>
2024-10-30 22:59:18 +0500

    Delete 6.8. Практическая работа directory

--- 6.8. Практическая работа/task4/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#testText').onclick = function() {
-    const userText = prompt('Введите новый текст ссылки:');
-    const testText = document.querySelector('#testText');
-    testText.textContent = userText;
-}
